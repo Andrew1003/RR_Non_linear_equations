0709522da4cbb000b54f792d80d2de12efdccfdd
Andrew1003 <101256289+Andrew1003@users.noreply.github.com>
2023-12-14 14:47:56 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,13 +1,1 @@
-# rr_nonlinear_solver/nonlinear_solver.py
-
-import numpy as np
-
-class NonlinearSolver:
-    def __init__(self, equation):
-        self.equation = equation
-
-    def solve(self, initial_guess):
-        # Ваш код для розв'язання нелінійного рівняння
-        pass
-
-# Інші допоміжні функції та класи можна додати тут
+# rr_nonlinear_solver/__init__.py
